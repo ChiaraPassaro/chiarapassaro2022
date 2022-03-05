0f5394a2013fdbbf0df08c634746acf4c0174960
Chiara Passaro <chiarapassaro.wd@gmail.com>
2022-03-05 15:55:41 +0100

    add waves

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <header>
+    <Header></Header>
+
+    <!-- <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div> -->
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import "@/assets/style.scss";
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Wave from "./Wave.vue";
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <header class="header d-flex align-items-center">
+    <h1 class="header__title">Chiara Passaro</h1>
+    <h2 class="header__subtitle">
+      Team Manager, Teacher and <br />
+      FULL STACK WEB DEVELOPER
+    </h2>
+    <Wave class="wave"></Wave>
+  </header>
+</template>
+
+<style lang="scss">
+@use "@/assets/partials/_commons.scss";
+.wave {
+  position: relative;
+}
+</style>
