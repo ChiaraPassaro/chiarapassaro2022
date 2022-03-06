d36d07743602abf829e585c7aa68581e92423b1a
Chiara Passaro <chiarapassaro.wd@gmail.com>
2022-03-06 15:44:28 +0100

    responsive scss

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,11 @@
 
 <template>
   <header class="header">
-    <div class="header__top">
-      <h1 class="header__title">Chiara Passaro</h1>
-      <h2 class="header__subtitle">
-        Team Manager, Teacher and <br />
-        FULL STACK WEB DEVELOPER
-      </h2>
-    </div>
+    <h1 class="header__title">Chiara Passaro</h1>
+    <h2 class="header__subtitle xs-mb-2">
+      Team Manager, Teacher and <br />
+      FULL STACK WEB DEVELOPER
+    </h2>
   </header>
 </template>
 
