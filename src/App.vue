8623cfe8af71d9addbe99865f7b08707855e8f3f
Chiara Passaro <chiarapassaro.wd@gmail.com>
2022-03-13 15:54:55 +0100

    add gradient palette

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <script setup>
+import ColorPalettesRange from "@chiarapassaro/color-palettes-range/src/js";
+
 // import { RouterLink, RouterView } from "vue-router";
 import Header from "@/components/Header.vue";
 import Wave from "@/components/Wave.vue";
 import { computed } from "vue";
 
+//rust color
+const BrownRust = new ColorPalettesRange.Hsl({
+  hue: 21,
+  saturation: 46,
+  brightness: 49,
+});
+
+const CadetBlue = new ColorPalettesRange.Hsl({
+  hue: 174,
+  saturation: 25,
+  brightness: 56,
+});
+
+//TODO Color Palette add generate gradient from two colors - this is for waves
+
+//TODO Genarate from this two colors different palettes
+//TODO complementary rage and pick first and last
 const colors = [
   {
     start: "#b46a41",
     stop: "#66a59f",
   },
   {
-    start: "#b42b41",
-    stop: "#3b65b0",
+    start: "#b69b54",
+    stop: "#b654af",
   },
   {
     start: "#b05858",
@@ -19,6 +38,7 @@ const colors = [
   },
 ];
 
+// TODO change colors
 const waveColors = {
   wave1: [
     "#c85b28",
@@ -35,14 +55,12 @@ const waveColors = {
 };
 
 const start = computed(() => {
-  return colors[0].start;
+  return colors[1].start;
 });
 
 const stop = computed(() => {
-  return colors[0].stop;
+  return colors[1].stop;
 });
-
-//todo modify colors
 </script>
 <template>
   <div class="container" :style="`--start: ${start}; --stop: ${stop};`">
@@ -61,9 +79,7 @@ const stop = computed(() => {
 </template>
 
 <style lang="scss">
-$xs: 798px;
-$sm: 1200px;
-
+//TODO add min height
 @mixin title {
   background: radial-gradient(circle, var(--start) 0%, var(--stop) 100%);
   background-clip: text;
@@ -73,6 +89,10 @@ $sm: 1200px;
 
   text-align: right;
 }
+
+// variables
+$xs: 798px;
+$sm: 1200px;
 
 :root {
   --bg-header: #fff;
@@ -97,6 +117,7 @@ body {
   font-size: 1vmax;
 }
 
+// container top zindex
 .container {
   position: fixed;
   z-index: 1;
@@ -105,11 +126,95 @@ body {
   grid-template-rows: 15% 30% 8% 47%;
   height: 100vh;
   width: 100%;
+
   @media screen and (max-width: 678px) {
     grid-template-rows: 15% 50% 8% 27%;
   }
+
+  .header {
+    display: grid;
+    align-items: center;
+    grid-template-columns: 30% 1fr;
+    grid-column: 2 / 3;
+    grid-row: 1;
+    background-color: white;
+    gap: 1em;
+
+    @media screen and (max-width: $sm) {
+      grid-template-columns: auto;
+    }
+
+    &__title {
+      @include title;
+      grid-column-start: 1;
+      grid-row-start: 1;
+
+      @media screen and (max-width: $sm) {
+        text-align: left;
+        grid-column-start: auto;
+        grid-row-start: auto;
+      }
+
+      font-weight: 400;
+      font-size: 2.5em;
+      line-height: 2em;
+
+      @media screen and (max-width: $sm) {
+        line-height: 1em;
+      }
+    }
+
+    &__subtitle {
+      grid-column-start: 2;
+      grid-row-start: 1;
+      margin-top: 0.5em;
+      font-weight: 400;
+      font-size: 1em;
+      line-height: 1.1em;
+      color: var(--orange);
+
+      @media screen and (max-width: $sm) {
+        text-align: left;
+        grid-column-start: auto;
+        grid-row-start: auto;
+      }
+    }
+  }
+
+  .wave {
+    display: grid;
+    grid-column: 1 / 4;
+    grid-row: 3 / 5;
+    background-color: white;
+    height: 100%;
+
+    @media screen and (max-width: $sm) {
+      position: relative;
+      grid-column: 3/5;
+      grid-row: 1/5;
+      .wave-container {
+        transform: rotate(90deg);
+        transform-origin: left top;
+        position: absolute;
+        z-index: 1;
+        top: 0;
+        right: -100vmax;
+        width: 100vmax;
+      }
+    }
+  }
+
+  .footer {
+    display: grid;
+    z-index: 0;
+    grid-column: 2 / 3;
+    grid-row: 4;
+    grid-template-columns: 30% 70%;
+    background-color: white;
+  }
 }
 
+// Container bottom zindex text content
 .container-bottom {
   display: grid;
   grid-template-columns: 5% 80% 15%;
@@ -118,115 +223,33 @@ body {
   position: relative;
   height: 120vh;
   margin-bottom: 30%;
-}
 
-.header {
-  display: grid;
-  align-items: center;
-  grid-template-columns: 30% 1fr;
-  grid-column: 2 / 3;
-  grid-row: 1;
-  background-color: white;
-  gap: 1em;
-
-  @media screen and (max-width: $sm) {
-    grid-template-columns: auto;
-  }
-
-  &__title {
-    @include title;
-    grid-column-start: 1;
-    grid-row-start: 1;
+  .main {
+    padding-bottom: 100vh;
+    grid-column: 2 / 3;
+    grid-row-start: 2;
+    grid-row-end: 5;
+    gap: 1em;
+    z-index: 0;
+    display: grid;
+    grid-template-columns: 30% 1fr;
 
     @media screen and (max-width: $sm) {
-      text-align: left;
-      grid-column-start: auto;
-      grid-row-start: auto;
-    }
-
-    font-weight: 400;
-    font-size: 2.5em;
-    line-height: 2em;
-
-    @media screen and (max-width: $sm) {
-      line-height: 1em;
+      grid-template-columns: auto;
     }
   }
 
-  &__subtitle {
-    grid-column-start: 2;
-    grid-row-start: 1;
-    margin-top: 0.5em;
-    font-weight: 400;
-    font-size: 1em;
-    line-height: 1.1em;
-    color: var(--orange);
+  .content {
+    &__title {
+      @include title;
+      @media screen and (max-width: $sm) {
+        text-align: left;
+        padding-top: 1em;
+      }
 
-    @media screen and (max-width: $sm) {
-      text-align: left;
-      grid-column-start: auto;
-      grid-row-start: auto;
+      font-weight: 400;
+      font-size: 1.8em;
     }
   }
-}
-
-.main {
-  padding-bottom: 100vh;
-  grid-column: 2 / 3;
-  grid-row-start: 2;
-  grid-row-end: 5;
-  gap: 1em;
-  z-index: 0;
-  display: grid;
-  grid-template-columns: 30% 1fr;
-
-  @media screen and (max-width: $sm) {
-    grid-template-columns: auto;
-  }
-}
-
-.wave {
-  display: grid;
-  grid-column: 1 / 4;
-  grid-row: 3 / 5;
-  background-color: white;
-  height: 100%;
-
-  @media screen and (max-width: $sm) {
-    position: relative;
-    grid-column: 3/5;
-    grid-row: 1/5;
-    .wave-container {
-      transform: rotate(90deg);
-      transform-origin: left top;
-      position: absolute;
-      z-index: 1;
-      top: 0;
-      right: -100vmax;
-      width: 100vmax;
-    }
-  }
-}
-
-.content {
-  &__title {
-    @include title;
-    @media screen and (max-width: $sm) {
-      text-align: left;
-      padding-top: 1em;
-    }
-
-    font-weight: 400;
-    font-size: 1.8em;
-  }
-}
-
-.footer {
-  display: grid;
-  z-index: 0;
-  grid-column: 2 / 3;
-  grid-row: 4;
-  grid-template-columns: 30% 70%;
-  background-color: white;
 }
 </style>
