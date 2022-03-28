701fc61c16eb9eba6c918bc09195a952f4fc4d4c
Chiara Passaro <chiarapassaro.wd@gmail.com>
2022-03-28 07:41:25 +0200

    arrow

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ const app = createApp(App);
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fas);
 library.add(far);
 library.add(fab);
 import { dom } from "@fortawesome/fontawesome-svg-core";
